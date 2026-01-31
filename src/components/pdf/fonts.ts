1bf4a1d9c07bce004298a92a2c9ae2f1800b61b1
Pawel Szpiczakowski <ceo@lemonode.pl>
2026-01-31 13:32:50 +0100

    Update Roboto font URLs from HTTP to HTTPS. (vercel mixed content fix)

--- src/components/pdf/fonts.ts
+++ src/components/pdf/fonts.ts
@@ -5,12 +5,12 @@ Font.register({
   family: "Roboto",
   fonts: [
     {
-      src: "http://fonts.gstatic.com/s/roboto/v16/zN7GBFwfMP4uA6AR0HCoLQ.ttf",
+      src: "https://fonts.gstatic.com/s/roboto/v16/zN7GBFwfMP4uA6AR0HCoLQ.ttf",
       fontWeight: 400,
       fontStyle: "normal",
     },
     {
-      src: "http://fonts.gstatic.com/s/roboto/v16/zN7GBFwfMP4uA6AR0HCoLQ.ttf",
+      src: "https://fonts.gstatic.com/s/roboto/v16/zN7GBFwfMP4uA6AR0HCoLQ.ttf",
       fontWeight: 700,
       fontStyle: "normal",
     },
